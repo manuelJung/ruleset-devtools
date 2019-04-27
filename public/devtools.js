22c1369a04bbc9371b8aa3f1c7f87ab859a28de0
manuelJung <manu.jung@relaxcommerce.com>
2019-04-27 23:20:11 +0200

    init devtools extension

---
+++ public/devtools.js
@@ -0,0 +1,8 @@
+chrome.devtools.panels.create("Ruleset",
+    "favicon.ico",
+    "index.html",
+    function(panel) {
+      // code invoked on panel creation
+      console.log('created')
+    }
+)
