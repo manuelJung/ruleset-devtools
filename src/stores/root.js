8c90eda028d7ddb8b88e90cee3dc15bf8b98ec2f
manuelJung <manu.jung@relaxcommerce.com>
2019-11-30 20:52:44 +0100

    bugfixed

--- src/stores/root.js
+++ src/stores/root.js
@@ -105,7 +105,7 @@ const rootStore:RootStore = observable(({
       byRuleExecId: {},
     },
     dispatchedActions: {
-      byDispatchedActionId: {}
+      byActionExecId: {}
     },
     ruleExecutions: {
       byRuleExecId: {},
