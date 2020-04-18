55ee18fcaa1750f9c2abe6cbee69cda437423801
manuelJung <manu.jung@relaxcommerce.com>
2020-04-18 22:40:26 +0200

    fixed console error

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,102 +1,104 @@
+;(function () {
 
 // COMUNICATION
 
 // console.log('mount-contentScript')
-let psBuffer = []
-let psMount = false
+  let psBuffer = []
+  let psMount = false
 
-// query tab
+  // query tab
 
-const queryTabCb = function (msg) {
-  if(msg.type === 'RESOLVE_TAB') {
-    chrome.runtime.onMessage.removeListener(queryTabCb)
-    // console.log('get tab', msg.tab)
-    setup(msg.tab)
-  }
-}
-chrome.runtime.onMessage.addListener(queryTabCb)
-chrome.runtime.sendMessage({type:'QUERY_TAB'})
-
-
-// notify redux-ruleset that it should generate events
-
-var observer = new MutationObserver(function () {
-  if(!document.head) return
-  observer.disconnect()
-
-  // add notifier
-  let n = document.createElement('script')
-  n.type = 'text/javascript'
-  n.innerText = 'window.RULESET_DEVTOOLS=true'
-  document.head.appendChild(n)
-});
-observer.observe(document, {
-  childList: true, // report added/removed nodes
-  subtree: true,   // observe any descendant elements
-})
-
-
-
-// setup communication
-
-function setup (tab) {
-
-  function sendToBackgroundScript (message) {
-    message.tab = tab
-    // console.log('send-to-bg', message)
-    chrome.runtime.sendMessage(message)
-  }
-
-  function sendToPageScript (message) {
-    if(!psMount) {
-      psBuffer.push(message)
-      return
+  const queryTabCb = function (msg) {
+    if(msg.type === 'RESOLVE_TAB') {
+      chrome.runtime.onMessage.removeListener(queryTabCb)
+      // console.log('get tab', msg.tab)
+      setup(msg.tab)
     }
-    // console.log('send-to-ps', message)
-    window.postMessage(message, '*')
   }
+  chrome.runtime.onMessage.addListener(queryTabCb)
+  chrome.runtime.sendMessage({type:'QUERY_TAB'})
 
-  // recieveFromPageScript
-  window.addEventListener('message', message => {
-    if(typeof message.data !== 'object') return
-    if(!message.data.isRulesetMessage) return
-    if(message.data.direction !== 'bottom-up') return
-    // console.log('get-from-ps', message)
-    sendToBackgroundScript(message.data)
-  }, false)
 
-  // recieveFromBackgroundScript
-  chrome.runtime.onMessage.addListener(message => {
-    // console.log('get-from-bg', message)
-    sendToPageScript(message)
+  // notify redux-ruleset that it should generate events
+
+  var observer = new MutationObserver(function () {
+    if(!document.head) return
+    observer.disconnect()
+
+    // add notifier
+    let n = document.createElement('script')
+    n.type = 'text/javascript'
+    n.innerText = 'window.RULESET_DEVTOOLS=true'
+    document.head.appendChild(n)
+  });
+  observer.observe(document, {
+    childList: true, // report added/removed nodes
+    subtree: true,   // observe any descendant elements
   })
 
-window.addEventListener('beforeunload', (event) => {
+
+
+  // setup communication
+
+  function setup (tab) {
+
+    function sendToBackgroundScript (message) {
+      message.tab = tab
+      // console.log('send-to-bg', message)
+      chrome.runtime.sendMessage(message)
+    }
+
+    function sendToPageScript (message) {
+      if(!psMount) {
+        psBuffer.push(message)
+        return
+      }
+      // console.log('send-to-ps', message)
+      window.postMessage(message, '*')
+    }
+
+    // recieveFromPageScript
+    window.addEventListener('message', message => {
+      if(typeof message.data !== 'object') return
+      if(!message.data.isRulesetMessage) return
+      if(message.data.direction !== 'bottom-up') return
+      // console.log('get-from-ps', message)
+      sendToBackgroundScript(message.data)
+    }, false)
+
+    // recieveFromBackgroundScript
+    chrome.runtime.onMessage.addListener(message => {
+      // console.log('get-from-bg', message)
+      sendToPageScript(message)
+    })
+
+  window.addEventListener('beforeunload', (event) => {
+    sendToBackgroundScript({
+      type: 'UNLOAD_PAGE',
+      isRulesetMessage: true,
+      direction: 'bottom-up',
+    })
+  })
   sendToBackgroundScript({
-    type: 'UNLOAD_PAGE',
+    type: 'MOUNT_PAGE',
     isRulesetMessage: true,
     direction: 'bottom-up',
   })
-})
-sendToBackgroundScript({
-  type: 'MOUNT_PAGE',
-  isRulesetMessage: true,
-  direction: 'bottom-up',
-})
 
 
-  // add pageScript
-  let s = document.createElement('script')
-  s.type = 'text/javascript'
-  s.async = true
-  s.src = chrome.extension.getURL('pageScript.js')
-  s.onload = function() {
-    this.parentNode.removeChild(this)
-    psMount = true
-    psBuffer.forEach(sendToPageScript)
-    psBuffer = []
+    // add pageScript
+    let s = document.createElement('script')
+    s.type = 'text/javascript'
+    s.async = true
+    s.src = chrome.extension.getURL('pageScript.js')
+    s.onload = function() {
+      this.parentNode.removeChild(this)
+      psMount = true
+      psBuffer.forEach(sendToPageScript)
+      psBuffer = []
+    }
+    
+    document.head.appendChild(s)
+    // console.log('setup finished')
   }
-  
-  document.head.appendChild(s)
-  // console.log('setup finished')
-}
+})()
--- public/pageScript.js
+++ public/pageScript.js
@@ -1,62 +1,65 @@
+;(function () {
 
 // COMMUNICATION
 
 // console.log('mount-pageScript')
 
-function sendToContentScript (message) {
-  // console.log('send-to-content-script', message)
-  window.postMessage(message, '*')
-}
+  function sendToContentScript (message) {
+    // console.log('send-to-content-script', message)
+    window.postMessage(message, '*')
+  }
 
-function recieveFromContentScript (cb) {
-  window.addEventListener('message', message => {
+  function recieveFromContentScript (cb) {
+    window.addEventListener('message', message => {
+      if(typeof message.data !== 'object') return
+      if(!message.data.isRulesetMessage) return
+      if(message.data.direction !== 'top-down') return
+      cb(message)
+    }, false)
+  }
+
+  const sendEventsToDevtools = events => sendToContentScript({
+    isRulesetMessage: true,
+    type: 'UPDATE_RULESET_EVENTS',
+    direction: 'bottom-up',
+    events: JSON.parse(JSON.stringify(events, null, 2)),
+  })
+
+
+  // SCRIPT
+
+
+  let buffer = []
+  let active = false
+
+  window.__REDUX_RULESET_DEVTOOLS__ = function (event) {
+    if(!active) buffer.push(event)
+    else sendEventsToDevtools([event])
+  }
+
+  recieveFromContentScript(message => {
+    // console.log('recieve-from-content-script', message)
     if(typeof message.data !== 'object') return
-    if(!message.data.isRulesetMessage) return
-    if(message.data.direction !== 'top-down') return
-    cb(message)
-  }, false)
-}
-
-const sendEvents = events => sendToContentScript({
-  isRulesetMessage: true,
-  type: 'UPDATE_RULESET_EVENTS',
-  direction: 'bottom-up',
-  events: JSON.parse(JSON.stringify(events, null, 2)),
-})
-
-
-// SCRIPT
-
-
-let buffer = []
-let active = false
-
-window.__REDUX_RULESET_DEVTOOLS__ = function (event) {
-  if(!active) buffer.push(event)
-  else sendEvents([event])
-}
-
-recieveFromContentScript(message => {
-  // console.log('recieve-from-content-script', message)
-  if(typeof message.data !== 'object') return
-  if(message.data.type === 'OPEN_DEVTOOLS') {
-    active = true
-    if(buffer.length) {
-      sendEvents(buffer)
-      buffer = []
+    if(message.data.type === 'OPEN_DEVTOOLS') {
+      active = true
+      if(buffer.length) {
+        sendEventsToDevtools(buffer)
+        buffer = []
+      }
     }
-  }
-  if(message.data.type === 'CLOSE_DEVTOOLS') {
-    active = false
-  }
-})
+    if(message.data.type === 'CLOSE_DEVTOOLS') {
+      active = false
+    }
+  })
 
-// deprecated: old version
-setTimeout(() => {
-  if(window.__addRulesetEventListener){
-    window.__addRulesetEventListener(e => {
-      if(!active) buffer.push(e)
-      else sendEvents([e])
-    }, true)
-  }
-}, 1000)
+  // deprecated: old version
+  setTimeout(() => {
+    if(window.__addRulesetEventListener){
+      window.__addRulesetEventListener(e => {
+        if(!active) buffer.push(e)
+        else sendEventsToDevtools([e])
+      }, true)
+    }
+  }, 1000)
+})()
+
