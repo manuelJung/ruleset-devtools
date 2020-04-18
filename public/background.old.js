25c9616c8803432d2bdaa396cf2b149c75d67a41
manuelJung <manu.jung@relaxcommerce.com>
2020-04-18 22:22:40 +0200

    removed old background script

--- public/background.old.js
+++ public/background.old.js
@@ -1,60 +1,63 @@
+
+// console.log('mount background')
+
 // SETUP
 
-let connection = null
-let buffer = []
-
+// setup devtools connection
 chrome.runtime.onConnect.addListener(function (port) {
+  let buffer = []
+  let rootTab = null
+  // console.log('port', port)
   if(port.name !== 'Ruleset') return
-  connection = port
 
-  port.onDisconnect.addListener(function(port) {
-      sendToContentScript({type:'DISCONNECT'})
-      connection = null
-      port.onMessage.removeListener(sendToContentScript)
-  });
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    if(!tabs[0]) return
+    rootTab = tabs[0]
+  })
 
-  port.onMessage.addListener(sendToContentScript)
+  function onDisconnect () {
+    port.onMessage.removeListener(recieveFromDevtools)
+    port.onDisconnect.removeListener(onDisconnect)
+    chrome.runtime.onMessage.removeListener(recieveFromContentScript)
+  }
+
+  function recieveFromContentScript (msg) {
+    if(!msg.isRulesetMessage) return
+    if(!msg.tab || !rootTab) return
+    if(msg.tab.id !== rootTab.id) return
+    // console.log('recieve-from-content-script', msg)
+    sendToDevtools(msg)
+  }
+
+  function recieveFromDevtools (msg) {
+    // console.log('recieve-from-devtools', msg)
+    sendToContentScript(msg)
+  }
+
+  function sendToContentScript (msg) {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      if(!tabs[0]) return console.log('no active tab found')
+      chrome.tabs.sendMessage(tabs[0].id, msg)
+    })
+  }
+
+  function sendToDevtools (msg) {
+    port.postMessage(msg)
+  }
+
+  chrome.runtime.onMessage.addListener(recieveFromContentScript)
+  port.onDisconnect.addListener(onDisconnect)
+  port.onMessage.addListener(recieveFromDevtools)
   buffer.forEach(msg => sendToDevtools(msg))
   buffer = []
 })
 
 
-// COMMUNICATION
-
-function sendToDevtools (message) {
-  if(typeof message !== 'object') return
-  if(!message.isRulesetMessage) return
-  if(!connection) {
-    buffer.push(message)
-    return
-  }
-  connection.postMessage(message)
-}
-
-function sendToContentScript (message) {
+// send tab to content script
+chrome.runtime.onMessage.addListener((msg, {tab}, reply) => {
+  if(msg.type !== 'QUERY_TAB') return
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, message)
+    if(!tabs[0]) return
+    chrome.tabs.sendMessage(tabs[0].id, {type: 'RESOLVE_TAB',tab})
   })
-}
-
-function recieveFromContentScript (cb) {
-  chrome.runtime.onMessage.addListener(message => {
-    if(typeof message !== 'object') return
-    if(!message.isRulesetMessage) return
-    cb(message)
-  })
-}
-
-function recieveFromDevtools (cb) {}
-
-
-// setInterval(() => {
-//   console.log(typeof connection)
-// }, 1000)
-
-// SCRIPT
-
-
-recieveFromContentScript(message => {
-  sendToDevtools(message)
 })
